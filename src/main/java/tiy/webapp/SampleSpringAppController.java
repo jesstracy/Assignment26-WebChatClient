a19e6871329af62124f9988f642eb0852e958888
Jessica Tracy <j.tracy916@gmail.com>
2016-09-12 16:24:13 -0400

    Baseline done, but needs cleaning

--- src/main/java/tiy/webapp/SampleSpringAppController.java
+++ src/main/java/tiy/webapp/SampleSpringAppController.java
@@ -12,8 +12,6 @@ public class SampleSpringAppController {
     @RequestMapping(path = "/person-url", method = RequestMethod.GET)
     public String person(Model model, String name, String city, int age) {
 
-
-
         Person p = new Person(name, city, age);
         model.addAttribute("person-object", p);
         return "person-view";
@@ -25,28 +23,43 @@ public class SampleSpringAppController {
         return "home";
     }
 
-    @RequestMapping(path = "/logout", method = RequestMethod.GET)
-    public String logout(Model model, HttpSession session) {
-        session.removeAttribute("userName");
-        return "redirect:/";
-    }
-
     @RequestMapping(path = "/login", method = RequestMethod.POST)
     public String login(HttpSession session, String userName) {
         session.setAttribute("userName", userName);
         return "redirect:/";
     }
 
+    @RequestMapping(path = "/logout", method = RequestMethod.GET)
+    public String logout(Model model, HttpSession session) {
+        session.removeAttribute("userName");
+        return "redirect:/";
+    }
+
     // Input page
     @RequestMapping(path = "/chat", method = RequestMethod.GET)
-    public String input(Model model, HttpSession session) {
-        model.addAttribute("message", session.getAttribute("message"));
+    public String input(Model model, HttpSession session, String message) {
 
-        WebChatClient myWebChatClient = new WebChatClient();
-        String serverResponse = myWebChatClient.sendMessage(session.getAttribute("message").toString());
-        System.out.println(serverResponse);
+//        session.setAttribute("message", message);
+//        model.addAttribute("message", session.getAttribute("message"));
+
+//        WebChatClient myWebChatClient = new WebChatClient();
+//        String serverResponse = myWebChatClient.sendMessage(message);
+//        System.out.println(serverResponse);
+
+
+//        model.addAttribute("message", session.getAttribute("message"));
 
         return "input";
+    }
+
+    @RequestMapping(path = "/send", method = RequestMethod.POST)
+    public String send(HttpSession session, String message) {
+        session.setAttribute("message", message);
+        System.out.println("Message: " + message);
+        WebChatClient myWebChatClient = new WebChatClient();
+        String serverResponse = myWebChatClient.sendMessage(message);
+        System.out.println(serverResponse);
+        return "redirect:/chat";
     }
 
 }
